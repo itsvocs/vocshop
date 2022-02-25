cd0d3e91109ff4432447d73df606aeb61f95ccb0
KINGPVJS <jordanpouani@icloud.com>
2022-02-25 08:50:11 +0100

    share code on github

---
+++ frontend/src/component/home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from "../layout/Header"
+import Product from "../product/Product"
+export default function Home() {
+  return (
+    <div>
+        <Header />
+        <Product />
+    </div>
+  )
+}
---
+++ frontend/src/data.js
@@ -0,0 +1,69 @@
+const data = {
+    products: [
+        {
+            _id: "1",
+            name: "Basic Tee shirt",
+            category: "shirt",
+            image: [
+                {
+                    _id: "image1",
+                    src: "/img/1.png"
+                }
+            ],
+            price: 8000,
+            rating:4.5,
+            numReviews: 10,
+            description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis enim odio culpa quasi! Voluptate quisquam illo veritatis quasi. Nisi adipisci velit aliquid laudantium dicta, a, cum placeat sunt ipsa corrupti hic veniam exercitationem odit inventore totam aspernatur quaerat quia in?",
+            color: "marron"
+        },
+        {
+            _id: "2",
+            name: "Basic Tee shirt",
+            category: "shirt",
+            image: [
+                {
+                    _id: "image2",
+                    src: "/img/2.png"
+                }
+            ],
+            price: 8000,
+            rating:4.5,
+            numReviews: 10,
+            description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis enim odio culpa quasi! Voluptate quisquam illo veritatis quasi. Nisi adipisci velit aliquid laudantium dicta, a, cum placeat sunt ipsa corrupti hic veniam exercitationem odit inventore totam aspernatur quaerat quia in?",
+            color: "marron"
+        },
+        {
+            _id: "3",
+            name: "ensemble vacance shirt",
+            category: "shirt",
+            image: [
+                {
+                    _id: "image3",
+                    src: "/img/3.png"
+                }
+            ],
+            price: 8000,
+            rating:4.5,
+            numReviews: 10,
+            description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis enim odio culpa quasi! Voluptate quisquam illo veritatis quasi. Nisi adipisci velit aliquid laudantium dicta, a, cum placeat sunt ipsa corrupti hic veniam exercitationem odit inventore totam aspernatur quaerat quia in?",
+            color: "marron"
+        },
+        {
+            _id: "4",
+            name: "Jordan Nike",
+            category: "shoes",
+            image: [
+                {
+                    _id: "image4",
+                    src: "/img/4.png"
+                }
+            ],
+            price: 8000,
+            rating:4.5,
+            numReviews: 10,
+            description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis enim odio culpa quasi! Voluptate quisquam illo veritatis quasi. Nisi adipisci velit aliquid laudantium dicta, a, cum placeat sunt ipsa corrupti hic veniam exercitationem odit inventore totam aspernatur quaerat quia in?",
+            color: "marron"
+        }
+    ]
+}
+export default data
